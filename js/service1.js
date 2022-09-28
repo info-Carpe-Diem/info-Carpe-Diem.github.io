58a120eee49233d92ea5622283662f0cf351d498
Nino <101824874+ninousenashvili@users.noreply.github.com>
2022-09-28 00:00:11 -0700

    navigation class name aded

--- js/service1.js
+++ js/service1.js
@@ -4,3 +4,19 @@ for (i = 0; i < accordeon.length; i++) {
     this.classList.toggle("active-question");
   });
 }
+
+// burger bar
+let hamburger = document.querySelector(".humburger");
+let menubar = document.querySelector(".nav-ul");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("activemenu");
+  menubar.classList.toggle("activemenu");
+});
+
+document.querySelectorAll(".nav-a").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("activemenu");
+    menubar.classList.remove("activemenu");
+  })
+);
