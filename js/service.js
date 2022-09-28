d0ddcc2fd4fb501e5dbcdfdc1515e9081884196c
Nino <101824874+ninousenashvili@users.noreply.github.com>
2022-09-27 23:13:51 -0700

    responsive service page

---
+++ js/service.js
@@ -0,0 +1,15 @@
+// burger bar
+let hamburger = document.querySelector(".humburger");
+let menubar = document.querySelector(".nav-ul");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("activemenu");
+  menubar.classList.toggle("activemenu");
+});
+
+document.querySelectorAll(".nav-a").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("activemenu");
+    menubar.classList.remove("activemenu");
+  })
+);
