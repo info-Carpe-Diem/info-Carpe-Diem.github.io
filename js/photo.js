39ea3df7cd24fd21fc6beaef09ed1b96d2aa4e64
Nino <101824874+ninousenashvili@users.noreply.github.com>
2023-02-08 09:41:11 -0800

    preshool gallery added

--- js/photo.js
+++ js/photo.js
@@ -39,3 +39,31 @@ let date =
   "." +
   today.getFullYear();
 document.querySelector(".ftr-bottom-text").append(date);
+
+// slider
+// // slider
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+
+  // If we need pagination
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+  autoplay: {
+    delay: 2500,
+  },
+  loop: true,
+});
